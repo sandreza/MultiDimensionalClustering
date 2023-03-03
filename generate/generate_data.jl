c3e45ce2cd2c32eb5158000143e5aa418b542999
Andre Souza <andrenogueirasouza@gmail.com>
2023-03-02 19:57:00 -0500

    more comments

--- generate/generate_data.jl
+++ generate/generate_data.jl
@@ -6,12 +6,14 @@ Random.seed!(12345)
 # create data directory if it's not there
 isdir(pwd() * "/data") ? nothing : mkdir(pwd() * "/data")
 
+# potential well
 if isfile(pwd() * "/data/potential_well.hdf5")
     @info "potential well data already exists. skipping data generation"
 else 
     include("potential_well.jl")
 end
 
+# lorenz
 if isfile(pwd() * "/data/lorenz.hdf5")
     @info "lorenz data already exists. skipping data generation"
 else
