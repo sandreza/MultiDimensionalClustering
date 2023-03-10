e7fb93caab23e90820d76a5a3805e06156abcdd7
Andre Souza <andrenogueirasouza@gmail.com>
2023-03-10 16:53:35 -0500

    small modification to Lorenz cluster

--- cluster/lorenz_cluster.jl
+++ cluster/lorenz_cluster.jl
@@ -12,7 +12,7 @@ dt = read(hfile["dt"])
 close(hfile)
 ##
 @info "applying K-means"
-kmn = kmeans(x, 1000; max_iters=10^6);
+kmn = kmeans(x, 1200; max_iters=10^6);
 X = kmn.assignments
 ##
 @info "Computing Generator and Eigenvalue Decomposition"
@@ -29,19 +29,20 @@ q_min = 1e-16
 P = exp(Q * τ)
 F, G, H = leicht_newman(P, q_min)
 @info "The algorithm identified $(length(F)) clusters"
-X_LN = classes_timeseries(F, X)
+X_LN = classes_timeseries(F, X);
 ##
 @info "plotting"
 # https://juliagraphics.github.io/ColorSchemes.jl/stable/catalogue/ 
 # for colorschemes
+set_theme!(backgroundcolor=:black)
 fig = Figure(resolution=(1707, 885))
 ax_old = LScene(fig[1, 1]; show_axis=false)
 ax_new = LScene(fig[1, 2]; show_axis=false)
-set_theme!(backgroundcolor=:black)
 lines!(ax_old, x[1, :], x[2, :], x[3, :], color=X, colormap=:glasbey_hv_n256, markersize=20.0, markerspacing=0.1, markerstrokewidth=0.0)
 lines!(ax_new, x[1, :], x[2, :], x[3, :], color=X_LN, colormap=:glasbey_hv_n256, markersize=20.0, markerspacing=0.1, markerstrokewidth=0.0)
 display(fig)
 ##
+set_theme!(backgroundcolor=:white)
 fig_trajectory = Figure()
 ax1 = Axis(fig_trajectory[1, 1]; title="x")
 ax2 = Axis(fig_trajectory[2, 1]; title="y")
