a58f007fe1781f375d036497d433102f16e1e878
Andre Souza <andrenogueirasouza@gmail.com>
2023-03-17 17:54:13 -0400

    potential picture

--- cluster/potential_well_cluster.jl
+++ cluster/potential_well_cluster.jl
@@ -65,3 +65,34 @@ for i in 1:3
     scatter!(ax, gs[3-i + 1], color=X_LN[1:timesteps], colormap=:glasbey_hv_n256, markersize=20.0, markerspacing=0.1, markerstrokewidth=0.0)
 end
 display(fig_trajectory)
+##
+# number of clusters a function of qmin and τ
+τs = [dt, 1.0 / -real(Λ[end-3]), 1.0 / -real(Λ[end-2]), 1.0 / -real(Λ[end-1])]
+noc = Vector{Int64}[]
+qmins = [10.0^i for i in -6:0.05:3]
+
+for τ in ProgressBar(τs)
+    P = exp(Qnew * τ)
+    number_of_clusters = Int64[]
+    for qmin in ProgressBar(qmins)
+        F, G, H = leicht_newman(P, qmin)
+        push!(number_of_clusters, length(F))
+    end
+    push!(noc, number_of_clusters)
+end
+##
+τ_labels = ["τ = dt", "τ = -1 / λ₄", "τ = -1 / λ₃", "τ = -1 / λ₂"]
+fig = Figure() 
+labelsize = 40
+axis_options = (; xlabel="log10(qmin)", ylabel="Number of Clusters", xgridstyle=:dash, ygridstyle=:dash, ygridwidth=5, xgridwidth=5, titlesize=labelsize, ylabelsize=labelsize, xlabelsize=labelsize, xticklabelsize=labelsize, yticklabelsize=labelsize)
+for i in 1:4
+    jj = (i-1) % 2 + 1
+    ii = (i-1) ÷ 2 + 1
+    ax = Axis(fig[ii, jj]; title = τ_labels[i], axis_options...)
+    scatter!(ax, log10.(qmins),  noc[i], markersize = 20.0, markerspacing = 0.1, markerstrokewidth = 0.0)
+    xlims!(ax, -7, 4)
+    ax.xticks = (collect(-7:4), string.(collect(-7:4)))
+    ax.yticks = (collect(1:8), string.(collect(1:8))) # , ["75S", "50S", "25S", "0", "25N", "50N", "75N"]
+    ylims!(ax, 0, 9)
+end
+display(fig)
