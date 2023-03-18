86d9a29c2419d3aa11c9aabdda9f6bf05f77e673
giorginiludovico <57105365+giorginiludovico@users.noreply.github.com>
2023-03-18 16:31:59 +0100

    Add files via upload
    Added Kuramoto and PIV data. Problems with GoogleDrive.jl.

--- generate/generate_data.jl
+++ generate/generate_data.jl
@@ -1,4 +1,4 @@
-using MultiDimensionalClustering, Random, Enzyme, HDF5, ProgressBars
+using MultiDimensionalClustering, Random, Enzyme, HDF5, ProgressBars, GoogleDrive
 
 # random seed for reproducibility
 Random.seed!(12345)
@@ -20,11 +20,24 @@ else
     include("lorenz.jl")
 end
 
-# newton
+# newton-leipnik
 if isfile(pwd() * "/data/newton.hdf5")
     @info "newton data already exists. skipping data generation"
 else
     include("newton.jl")
 end
 
+# There are problems with the GoogleDrive.jl package...
+# kuramoto
+if isfile(pwd() * "/data/newton.hdf5")
+    @info "kuramoto data already exists. skipping data generation"
+else
+    drive_download("https://drive.google.com/file/d/11FN55Kr0Ue3JI5RDon60Iwquvoc-j5SW/view?usp=share_link", pwd() * "/data/kuramoto.hdf5")
+end
 
+# PIV
+if isfile(pwd() * "/data/PIV.hdf5")
+    @info "PIV data already exists. skipping data generation"
+else
+    drive_download("https://drive.google.com/file/d/11M2fCihaDXdW-_Kzm2XWChKP0qqOXDBp/view?usp=share_link", pwd() * "/data/PIV.hdf5")
+end
