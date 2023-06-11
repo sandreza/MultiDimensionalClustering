9c64a7971a39d3a5d6bb40666e0e9f6ece06e139
Andre Souza <andrenogueirasouza@gmail.com>
2023-06-11 18:35:27 -0400

    update data download

--- download/data.jl
+++ download/data.jl
@@ -1,16 +1,12 @@
 using ZipFile
 using HTTP 
+using ProgressBars
 
-
-using HTTP
 url = "https://dl.dropboxusercontent.com/s/xjkaebigky34i21/data.zip"
 HTTP.download(url, "data.zip")
 
-using ZipFile
-
 z = ZipFile.Reader("data.zip")  # open the zip file
-
-for f in z.files  # loop over the files in the zip file
+for f in ProgressBar(z.files)  # loop over the files in the zip file
     if f.name == "data/" 
         isdir(pwd() * "/data") ? nothing : mkdir(pwd() * "/data")
     elseif f.name[1:5] != "data/"
